8a7d91eeca05e0d4751d7a5cc381850913039aeb
Hasan Zaigam <121241913+HasanZaigam@users.noreply.github.com>
2025-02-15 15:45:38 +0530

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -54,7 +54,7 @@ const Projects = () => {
       : projectsData.filter((project) => project.category === selectedCategory);
 
   return (
-    <div className="flex flex-col items-center justify-center py-20 z-[20]" id="projects">
+    <div className="flex flex-col items-center justify-center py-10 z-[20]" id="projects">
       <h1 className="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10">
         My Projects
       </h1>
